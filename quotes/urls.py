9f34315926510ef2d96e7ebbffb334d5b73eb6c1
tilly-bu <tilahung@bu.edu>
2020-11-03 21:59:55 -0500

    qoutes 2.1

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from .views import HomePageView  #our view class definition -> connects the model to the template 
+from .views import HomePageView, QuotePageView, RandomQuotePageView
+ #our view class definition -> connects the model to the template 
 
 urlpatterns =  [
-    path('', HomePageView.as_view(), name="home_page"),
+    path('', RandomQuotePageView.as_view(), name="random"),
+    path('all', HomePageView.as_view(), name="all_quotes"),
+    path('quote/<int:pk>', QuotePageView.as_view(), name="quote"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView , DetailView
 from .models import Quote
+import random
 
 class HomePageView(ListView): #present many objects from one model in a single screen? 
     """show a listing of Quotes."""
@@ -10,6 +11,28 @@ class HomePageView(ListView): #present many objects from one model in a single s
 
     #context object name = name of variable from the database that we can call to deliver the data
 
+class QuotePageView(DetailView): 
+    '''Display a single qoute item'''
+    model = Quote #retrive Quote obejcts from the model database 
+    template_name = 'quotes/quote.html' #provide the name of the template we will use to display the data and create file 'home'
+    context_object_name = "quote" 
+
+class RandomQuotePageView(DetailView): 
+    '''Display a single qoute item'''
+    model = Quote #retrive Quote obejcts from the model database 
+    template_name = 'quotes/quote.html' #provide the name of the template we will use to display the data and create file 'home'
+    context_object_name = "quote" 
+
+    def get_object(self):
+        '''select a random quote to display in the quote.html template'''
+        #obtain all quotes using managet 
+        quotes = Quote.objects.all()
+        #select one at random 
+        q = random.choice(quotes) 
+        return q 
 
 
-# Create your views here.
+
+
+
+
