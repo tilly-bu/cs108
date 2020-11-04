240f994b94a3f4adf2460a90dbdc112cc25f8289
tilly-bu <tilahung@bu.edu>
2020-11-03 22:01:36 -0500

    quote2.2

--- quotes/views.py
+++ quotes/views.py
@@ -12,7 +12,7 @@ class HomePageView(ListView): #present many objects from one model in a single s
     #context object name = name of variable from the database that we can call to deliver the data
 
 class QuotePageView(DetailView): 
-    '''Display a single qoute item'''
+    '''Display a single quote item'''
     model = Quote #retrive Quote obejcts from the model database 
     template_name = 'quotes/quote.html' #provide the name of the template we will use to display the data and create file 'home'
     context_object_name = "quote" 
